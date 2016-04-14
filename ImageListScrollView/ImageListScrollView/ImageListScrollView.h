e9f64830ac3e051b464909d14cd0c9bb39a4e2c9
gs01md <gshzyfx@163.com>
2016-04-14 18:04:40 +0800

    导入SDWebImage库
    如果传入的是网络图片url ，则需要该库

---
+++ ImageListScrollView/ImageListScrollView/ImageListScrollView.h
@@ -0,0 +1,19 @@
+//
+//  ImageListScrollView.h
+//  ImageListScrollView
+//
+//  Created by Simon on 16/4/14.
+//  Copyright © 2016年 gaoshuai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ImageListScrollView.
+FOUNDATION_EXPORT double ImageListScrollViewVersionNumber;
+
+//! Project version string for ImageListScrollView.
+FOUNDATION_EXPORT const unsigned char ImageListScrollViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ImageListScrollView/PublicHeader.h>
+
+
